5b6791df7fb635681ebc6e5387a065ecb5d751b2
Brian Emilius <be@rts.dk>
2021-01-20 14:22:02 +0100

    add featured page

--- src/components/FeaturedCard.js
+++ src/components/FeaturedCard.js
@@ -1,9 +1,13 @@
-export default function FeaturedCard() {
+import "./FeaturedCard.scss";
+
+export default function FeaturedCard({ image, album, genre }) {
 	return (
-		<article>
-			<img src="https://via.placeholder.com/300x400" alt="" />
-			<p>Artist</p>
-			<p>Title</p>
+		<article className="featuredCard">
+			<img src={image} className="featuredCard__image" alt={album} />
+			<div className="featuredCard__text">
+				<p className="featuredCard__album">{album}</p>
+				<p className="featuredCard__genre">{genre}</p>
+			</div>
 		</article>
 	);
 }
