a14956938aa02c1a794a7a909d5458c806151295
Brian Emilius <be@rts.dk>
2021-01-29 10:06:15 +0100

    implement login flow

--- src/App.js
+++ src/App.js
@@ -3,16 +3,34 @@ import { Router } from "@reach/router";
 import Featured from "./pages/Featured";
 import Playlists from './pages/Playlists';
 import Player from './pages/Player';
+import Login from './pages/Login';
+import TokenContext from "./contexts/TokenContext";
+import { useState } from "react";
+import Callback from './pages/Callback';
 
 function App() {
+  var tokenState = useState(null);
+
   return (
-    <div className="app">
-      <Router>
-        <Featured path="/featured" />
-        <Playlists path="/playlists" />
-        <Player path="/player" />
-      </Router>
-    </div>
+    <TokenContext.Provider value={tokenState}>
+      <div className="app">
+        <Router>
+          {(function() {
+            if (tokenState[0]) {
+              return (
+                <>
+                  <Featured path="/featured" />
+                  <Playlists path="/playlists" />
+                  <Player path="/player" />
+                </>
+              )
+            }
+          }())}
+          <Login default />
+          <Callback path="/callback" />
+        </Router>
+      </div>
+    </TokenContext.Provider>
   );
 }
 
